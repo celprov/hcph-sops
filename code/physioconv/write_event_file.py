ec3191357c2ec82facc96561911b1384109add30
Celine Provins <celine.provins@unil.ch>
2023-10-19 12:36:19 -0700

    enh: generate events.tsv file from psychopy log

--- code/physioconv/write_event_file.py
+++ code/physioconv/write_event_file.py
@@ -22,6 +22,7 @@ import pandas as pd
 import os
 import gzip
 import json
+import re
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
@@ -33,6 +34,32 @@ EVENTS_JSON_BOILERPLATE = {
         "SoftwareVersion": "2022.3.0.dev6",
     }
 }
+
+TRIAL_TYPE = {
+    "bh_body_2": "hold",
+    "bh_body": "hold-test",
+    "bh_end": "hold-end-test",
+    "bh_end_2": "hold-end",
+    "bh_end_3": "breath-freely",
+    "eye_movement_fixation": "cog",
+    "ft_hand": "mot",
+    "fixation": "blank",
+    "grating": "vis",
+    "movie": "movie",
+    "polygon_4": "breath-in",
+    "polygon_5": "breath-out",
+    "polygon_6":"breath-in-last",
+    "polygon_8": "breath-out-last",
+    "polygon_7": "breath-out-last",   
+}
+
+
+class PatternNotFoundError(Exception):
+    def __init__(self, pattern, log):
+        super().__init__(
+            f"The pattern {pattern} was not found in the log. Please check that the input {log} is a log output by Psychopy and that it
+            does not correspond to a task that was aborted."
+        )
 
 
 def plot_physio_data_with_events(
@@ -97,7 +124,7 @@ def plot_physio_data_with_events(
     return output_file
 
 
-def write_event_file(tsv_file: str) -> None:
+def write_event_file_from_channels(tsv_file: str) -> None:
     """
     Create a BIDS events file from the digital channels of the physiological file.
 
@@ -231,8 +258,10 @@ Finger taping with the left or right hand following the indications on the scree
         }
     output_folder = os.path.dirname(tsv_file)
     base_name = os.path.basename(tsv_file)
+    # The from_channels might break the BIDS compatibility, but for now I don't know how else
+    # to distinguish between events.tsv generated from the channels versus the psychopy log
     output_file = os.path.join(
-        output_folder, base_name.replace("_physio.tsv.gz", "_events.tsv")
+        output_folder, base_name.replace("_physio.tsv.gz", "_from_channels_events.tsv")
     )
     event_dataframe.to_csv(output_file, sep="\t", index=False)
     print(f"Event DataFrame saved to {output_file}")
@@ -243,6 +272,103 @@ Finger taping with the left or right hand following the indications on the scree
     print(f"JSON metadata saved to {json_file}")
 
     plot_physio_data_with_events(df, event_dataframe, tsv_file)
+
+
+def write_event_file_from_log(log: str) -> None:
+    """
+    Create a BIDS events file from the psychopy log.
+
+    Parameters
+    ----------
+    log :obj:`os.pathlike`
+         The path to the log output from psychopy.
+
+    """
+    with open(log, "r") as f:
+        file = f.read()
+
+        # Initialize events dataframe
+        event_dataframe = pd.DataFrame(
+            columns=["onset", "duration", "trial-type", "value"]
+        )
+
+        # Create a regular expression pattern to match lines containing any of the word corresponding to tasks
+        autodraw_pattern = r'(\d+\.\d+)[^\n]+({})\s*:\s*autoDraw\s*=\s*(True|False)'.format('|'.join(TRIAL_TYPE.keys()))
+
+        # Use re.findall to find all matching lines in the log
+        autodraw_events = re.findall(autodraw_pattern, file)
+
+        # Initialize variable to keep track of the start timestampss
+        start_timestamp = {}
+
+        # Extract the times associated with the matches
+        if autodraw_events:
+            for timestamp, keyword, status in autodraw_events:
+                if status == "True":
+                    # Store the start timestamp for "autoDraw=True"
+                    start_timestamp[keyword] = timestamp
+
+                elif status == "False" and keyword in start_timestamp:
+
+                    # Calculate the duration for "autoDraw=False" if there is a corresponding "autoDraw=True"
+                    onset = float(start_timestamp[keyword])
+                    end = float(timestamp)
+                    duration = end - onset
+
+                    # Match keyword with associated sub-task
+                    trial_type = TRIAL_TYPE[keyword]
+
+                    # For the fingertapping and the eye movement sub-tasks, we have to encode which hand or the position
+                    # of the fixation point to fully characterize the sub-task instance.
+                    value = ""
+                    if trial_type == 'mot':
+                        # Which hand is instructed to fingertap is encoded in the psychopy log one line above 
+                        # the onset 'ft_hand : autoDraw = True' and as the same timestamp as the onset.
+                        hand_pattern = r'{:.4f}\s+EXP\s+ft_hand:\s*text\s*=\s*\'(RIGHT|LEFT)\''.format(onset)
+                        hand_match = re.search(hand_pattern, file)
+                        value = hand_match.group(1).lower()
+
+                    elif trial_type == 'cog':
+                        # The position of the point is reported in the psychopy log 5 to 7 lines above 
+                        # the onset event of the cognitive instance. 
+
+                        # Retrieve line number corresponding to the onset event
+                        pattern = r'{:.4f}\s+EXP\s+{}:\s+autoDraw\s*=\s*True'.format(onset, keyword)
+                        match = re.search(pattern, file)
+                        start, end = match.span()  # Get the start and end position of the match
+                        line_nbr = file.count('\n', 0, start) + 1  # Calculate the corresponding line number
+
+                        # Extract the seven lines before the match
+                        previous_lines = file.split('\n')[line_nbr-7:line_nbr]
+                        previous_lines_text = '\n'.join(previous_lines)
+
+                        # Find all the matches using the regular expression
+                        fix_pos_pattern = r'([\d.]+)\s+EXP\s+New trial \(rep=\d+, index=\d+\): OrderedDict\(\[\(\'xpos\', (-?\d+\.\d+)\), \(\'ypos\', (-?\d+\.\d+)\)\]\)'
+                        matches = re.findall(fix_pos_pattern, previous_lines_text)
+                        # There should be only one match, otherwise we risk mixing up different instances
+                        assert len(matches) == 1
+                        timestamp, xpos, ypos = matches[0]
+                        value = f"[{xpos}, {ypos}]"
+
+
+                    # We have all the information needed for the event, it can be inserted in the dataframe.
+                    event = {"onset": onset, "duration": duration, "trial-type": trial_type, "value": value}
+                    event_dataframe = pd.concat([event_dataframe, pd.DataFrame([event])], ignore_index=True)
+
+                    # Remove the start timestamp from the dictionary to avoid double counting
+                    del start_timestamp[keyword]
+
+        else:
+            raise PatternNotFoundError(autodraw_pattern, log)
+
+        output_folder = os.path.dirname(log)
+        base_name = os.path.basename(log)
+        # The from_log might break the BIDS compatibility, but for now I don't know how else
+        # to distinguish between events.tsv generated from the channels versus the psychopy log
+        output_file = os.path.join(
+            output_folder, base_name.replace("_physio.tsv.gz", "_from_log_events.tsv")
+        )
+        event_dataframe.to_csv(output_file, sep="\t", index=False)
 
 
 def write_all_event_files(folder_path: str) -> None:
@@ -264,7 +390,13 @@ def write_all_event_files(folder_path: str) -> None:
 
     for filename in physio_files:
         file_path = os.path.join(folder_path, filename)
-        write_event_file(file_path)
+        write_event_file_from_channels(file_path)
+
+    physio_log = [filename for filename in file_list if ".log" in filename]
+
+    for filename in physio_log:
+        file_path = os.path.join(folder_path, filename)
+        write_event_file_from_log(file_path)
 
 
 if __name__ == "__main__":
