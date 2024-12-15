5d391d45e2dfe203df178f240331ee4db0d39978
Céline Provins <celine.provins@unil.ch>
2024-12-15 14:54:07 +0100

    fix: adapt bash scripts to run FC extraction on resting-state fMRI

--- code/fmri/launch_funconn.sh
+++ code/fmri/launch_funconn.sh
@@ -1,1 +1,1 @@
-python funconn.py /home/cprovins/data/hcph-derivatives/fmriprep-23.1.4-wp/ --task qct --ses 001 003 004 005 006 008 009
+python funconn.py /data/derivatives/hcph-derivatives-rsmovie/ --task rsmovie --ses 003
