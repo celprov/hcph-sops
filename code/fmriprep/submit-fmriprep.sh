ceccc8a374a6a4fd22be9d6d33e07102caaecd82
Céline Provins <77437752+celprov@users.noreply.github.com>
2025-01-28 10:16:21 +0100

    Apply suggestions from code review
    Co-authored-by: Oscar Esteban <code@oscaresteban.es>

--- code/fmriprep/submit-fmriprep.sh
+++ code/fmriprep/submit-fmriprep.sh
@@ -38,7 +38,7 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
-DATADIR=<workdir>/data/hcph-dataset
+DATADIR={{ secrets.data.curnagl_workdir | default('<workdir>') }}/data/hcph-dataset
 SUB="sub-001"
 pushd $DATADIR/inputs/$SUB > /dev/null
 ALL_SES=(`ls -d ses-*`)
