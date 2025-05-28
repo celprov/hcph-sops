a591d1b0af957af9d4886e0cfa66f4595689c7b6
Céline Provins <celine.provins@unil.ch>
2025-05-28 16:54:37 +0200

    enh: plot the residual evolution in function of caffeine consumption enh: try to run the analyses but considering only the connections that are NOT close to zero across all sessions

--- code/fmri/launch_funconn.sh
+++ code/fmri/launch_funconn.sh
@@ -1,1 +1,1 @@
-python funconn.py /data/derivatives/fmriprep-reliabilitypilot --task pilotrest --atlas-dimension 64 --ses pilot015
+python funconn.py /data/derivatives/fmriprep-reliabilitypilot --task pilotrest --atlas-dimension 64 --fc-estimator sparseinversecovariance --ses pilot015
